f9e6a1e3f61e95ec8c798e1c739c4eb6b40d664b
Sang Dang <sang@dang.to>
2025-03-27 18:35:29 +0200

    start converting normal list to view

--- main.go
+++ main.go
@@ -1,24 +1,39 @@
 package main
 
+// An example demonstrating an application with multiple views.
+//
+// Note that this example was produced before the Bubbles progress component
+// was available (github.com/charmbracelet/bubbles/progress) and thus, we're
+// implementing a progress bar from scratch here.
+
 import (
 	"fmt"
-	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+
+func main() {
+	initialModel := model{0, false, false}
+	p := tea.NewProgram(initialModel)
+	if _, err := p.Run(); err != nil {
+		fmt.Println("could not start program:", err)
+	}
+}
+
+// General stuff for styling the view
+var (
+	keywordStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("211"))
+	subtleStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("241"))
+	checkboxStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("212"))
+	mainStyle     = lipgloss.NewStyle().MarginLeft(2)
 )
 
 type model struct {
-	choices  []string
 	cursor   int
-	selected map[int]struct{}
-}
-
-func initiateAppModel() model {
-	return model{
-		choices:  []string{"Add new log", "View logs"},
-		cursor:   0,
-		selected: make(map[int]struct{}),
-	}
+	selected   bool
+	quit bool
 }
 
 func (m model) Init() tea.Cmd {
@@ -26,59 +41,106 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	if msg, ok := msg.(tea.KeyMsg); ok {
+		k := msg.String()
+		if k == "q" || k == "esc" || k == "ctrl+c" {
+			m.quit = true
+			return m, tea.Quit
+		}
+	}
+
+	// Hand off the message and model to the appropriate update function for the
+	// appropriate view based on the current state.
+	if !m.selected {
+		return updateChoices(msg, m)
+	}
+
+	return m, nil
+}
+
+// The main view, which just calls the appropriate sub-view
+func (m model) View() string {
+	var s string
+	if m.quit {
+		return "\n  See you later!\n\n"
+	}
+
+	if !m.selected {
+		s = choicesView(m)
+	} else {
+		s = chosenView(m)
+	}
+
+	return mainStyle.Render("\n" + s + "\n\n")
+}
+
+// Update loop for the first view where you're choosing a task.
+func updateChoices(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c", "q":
-			return m, tea.Quit
-		case "up", "k":
-			if m.cursor > 0 {
-				if m.cursor > 0 {
-					m.cursor--
-				}
+		case "j", "down":
+			m.cursor++
+			if m.cursor > 3 {
+				m.cursor = 3
 			}
-		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
-				m.cursor++
+		case "k", "up":
+			m.cursor--
+			if m.cursor < 0 {
+				m.cursor = 0
 			}
 		case "enter":
-			_, ok := m.selected[m.cursor]
-			if ok {
-				delete(m.selected, m.cursor)
-			} else {
-				m.selected[m.cursor] = struct{}{}
-			}
+			m.selected = true
+			return m, nil
 		}
 	}
 	return m, nil
 }
 
-func (m model) View() string {
-	s := "What do you want to do?\n"
+// Sub-views
 
-	for i, choice := range m.choices {
-		cursor := " "
-		if m.cursor == i {
-			cursor = ">"
-		}
+// The first view, where you're choosing a task
+func choicesView(m model) string {
+	c := m.cursor
 
-		checked := " "
-		if _, ok := m.selected[i]; ok {
-			checked = "x"
-		}
+	tpl := "What to do today?\n\n"
+	tpl += "%s\n\n"
+	tpl += subtleStyle.Render("j/k, up/down: select") + ", " +
+		subtleStyle.Render("enter: choose") + ", " +
+		subtleStyle.Render("q, esc: quit")
 
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
-	}
+	choices := fmt.Sprintf(
+		"%s\n%s\n%s\n%s",
+		item("Plant carrots", c == 0),
+		item("Go to the market", c == 1),
+		item("Read something", c == 2),
+		item("See friends", c == 3),
+	)
 
-	s += "\nPress q to quit\n"
-	return s
+	return fmt.Sprintf(tpl, choices)
 }
 
-func main() {
-	p := tea.NewProgram(initiateAppModel())
+// The second view, after a task has been chosen
+func chosenView(m model) string {
+	var msg string
 
-	if err := p.Start(); err != nil {
-		fmt.Printf("Some error occurred: %s\n", err)
-		os.Exit(1)
+	switch m.cursor {
+	case 0:
+		msg = fmt.Sprintf("Carrot planting?\n\nCool, we'll need %s and %s...", keywordStyle.Render("libgarden"), keywordStyle.Render("vegeutils"))
+	case 1:
+		msg = fmt.Sprintf("A trip to the market?\n\nOkay, then we should install %s and %s...", keywordStyle.Render("marketkit"), keywordStyle.Render("libshopping"))
+	case 2:
+		msg = fmt.Sprintf("Reading time?\n\nOkay, cool, then we’ll need a library. Yes, an %s.", keywordStyle.Render("actual library"))
+	default:
+		msg = fmt.Sprintf("It’s always good to see friends.\n\nFetching %s and %s...", keywordStyle.Render("social-skills"), keywordStyle.Render("conversationutils"))
 	}
+
+	return msg + "\n\n"
+}
+
+func item(label string, selected bool) string {
+	if selected {
+		return checkboxStyle.Render("[x] " + label)
+	}
+	return fmt.Sprintf("[ ] %s", label)
 }
